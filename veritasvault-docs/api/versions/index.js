b1b880d52bca81e190a39b3987906102d48dbb43
Jurie Smit <smit.jurie@gmail.com>
2025-05-11 07:15:13 +0200

    Update index.js

--- veritasvault-docs/api/versions/index.js
+++ veritasvault-docs/api/versions/index.js
@@ -1,6 +1,9 @@
 import { getVersionMetadata } from "../../src/utils/version"
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' })
+  }
   try {
     const metadata = await getVersionMetadata()
     return res.status(200).json(metadata)
