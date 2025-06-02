fe5f779e0299bb37e3625d3efdb62bb78c74eff3
JustAGhosT <smit.jurie@gmail.com>
2025-06-02 22:56:07 +0200

    This is a cleanup commit removing old implementation files that are no longer needed in the codebase.
    - Architecture page
    - Document versions page
    - Finance models page
    - Gaming technologies page
    - Governance framework page
    - Implementation roadmap page
    - Various integration pages
    - Security page
    - Technical infrastructure page
    - Tokenomics pages
    - PDF preview functionality

--- _backup/old_next_app/tokenomics-download/tokenomics-download-client-page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import TokenomicsDownloadPage from "@/components/tokenomics/tokenomics-download-page"
-
-export default function TokenomicsDownloadClientPage() {
-  return <TokenomicsDownloadPage />
-}
